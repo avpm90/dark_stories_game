7782ff37810f008e49261be1fcd4ab99422c667a
avpm <angelomartins90@gmail.com>
2022-06-13 18:24:00 -0300

    far

--- class.js
+++ class.js
@@ -1,46 +1,45 @@
-class Round {
-  constructor(darkStories) {
-    this.darkStories = darkStories;
-
-    const darkStories = [
+class Game {
+  constructor() {
+    this.darkStories = [
       {
         question:
           "A woman, in her apartment sadly watching the streets through her window. Soon after, she jumps out the window. At that exact moment she hears the phone ringing and regrets the jump.",
         correctAnswer:
           "After a nuclear disaster, the woman believes she is the only survivor and has chosen to commit suicide. When the phone rang she realized she wasn't alone. It was now, too late.",
-        option1:
+        options: [
           "A woman devastated by her divorce, wishing to end her own life jumped out the window. Immediately regretting her decision when hearing the ringtone set for her son play on her phone.",
-        option2:
+
           "After a nuclear disaster, the woman believes she is the only survivor and has chosen to commit suicide. When the phone rang she realized she wasn't alone. It was now, too late.",
-        option3:
+
           "A woman tired of waiting for her friends to call, jumped out the window, when she heard the phone ringing.",
+        ],
       },
       {
         question:
           "A man enters a bar and asks for a glass of water. The bartender pulls a gun from under the bar and points it at the man's head, who replies 'thanks' and leaves the bar.",
         correctAnswer:
           "A man had entered the bar to get rid of a bout of hiccups, looking for a glass of water. The bartender, realizing the situation, pulls out his gun and points it at the man in order to heal him with a scare.",
-        option1:
+        options: [
           "The man entered the bar looking for a job, and used the glass of water as an excuse, the bartender pointed the gun to him to test if he got easily scared, seeing he didn't gave the man a job as a security guard ",
-        option2:
+
           "The man first asked for water then, for a photo with the bartender pointing the gun at him, so he could miss work.",
-        option3:
+
           "The man had entered the bar to get rid of a bout of hiccups, looking for a glass of water. The bartender, realizing the situation, pulls out his gun and points it at the man in order to heal him with a scare.",
+        ],
       },
       {
         question:
           "Walking down a hallway, a man notices that a scream echoes through the place when the light flashes.",
         correctAnswer:
           "The man, walking in the prison corridor, next to the electric chair, hears the screams of the convicts in the chair.",
-        option1:
+        options: [
           "The man, walking in the prison corridor, close to the electric chair, hears the screams of the convicts about to die.",
-        option2:
+
           "Walking down his house the man heard his wife scream everytime the lights blinked.",
-        option3:
+
           "The man hear some noise coming from the other bedroom, walking down the hallway his kid tries to prank him as he turn on the light",
+        ],
       },
     ];
   }
-
-  questionsGenerator;
 }
--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-const startBtn = document.getElementById('start')
-startBtn.addEventListener('click', () => { 
-    
-})
+const games = new Game();
+console.log(games.darkStories[0].question);
 
+const gameBoard = document.getElementById("gameBoard");
+const question = document.getElementById("question");
+const startBtn = document.getElementById("start");
+const confirmBtn = document.getElementById("confirm");
 
+startBtn.addEventListener("click", () => {
+  printQuestions();
+  startBtn.classList.add("hide");
+});
+
+confirmBtn.addEventListener("click", () => {});
+
+function printQuestions() {
+  question.innerText = games.darkStories[0].question;
+
+  for (let i = 0; i < games.darkStories[0].options.length; i++) {
+    const button = document.createElement("button");
+    button.innerText = games.darkStories[0].options[i];
+    gameBoard.appendChild(button);
+
+    button.addEventListener("click", () => {
+      const clicked = event.target;
+      console.log(clicked);
+    });
+  }
+}
+
+function getRightOption() {}
