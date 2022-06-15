15672060c14d384d97e9bfd0b1fdb452ceed0bf7
avpm <angelomartins90@gmail.com>
2022-06-15 18:43:19 -0300

    changed buttons

--- index.js
+++ index.js
@@ -4,55 +4,42 @@ const gameBoard = document.getElementById("gameBoard");
 const question = document.getElementById("question");
 const startBtn = document.getElementById("start");
 const confirmBtn = document.getElementById("confirm");
+let currentElement = 0;
+const allOptions = document.getElementsByClassName("options");
 
 startBtn.addEventListener("click", () => {
   printQuestions(0);
   startBtn.classList.add("hide");
-  // confirmBtn.classList.add("hide");
 });
 
-//function goToNext() {
 confirmBtn.addEventListener("click", () => {
-  let currentElement = 0;
   if (currentElement < games.darkStories.length) {
     currentElement++;
   }
   printQuestions(currentElement);
 });
 
-// console.log(printQuestions(currentElement))
-//console.log(games.darkStories[currentElement].correctAnswer)
+// for está colocando o eventlistener em todas as opções e quando clicados, chama checkQ com a opção clicada
+for (let i = 0; i < allOptions.length; i++) {
+  allOptions[i].addEventListener("click", (event) => {
+    const clickedBtn = event.target.innerText;
+
+    checkQuestion(clickedBtn);
+  });
+}
 
 function printQuestions(currentElement) {
   question.innerText = games.darkStories[currentElement].question;
-  // confirmBtn.classList.add("hide");
-  // if (currentElement < games.darkStories.length) {
-  // button.parentNode.removeChild(button)}
-  //   gameBoard.classList.remove("allbtn");}
-  //   const all = document.querySelectorAll(".allBtn");
-  //all.forEach(allBtn => {
-  //allBtn.remove();
-  //})
-  // }
 
-  console.log(gameBoard);
   for (let i = 0; i < games.darkStories[currentElement].options.length; i++) {
-    const button = document.createElement("button");
+    allOptions[i].innerText = games.darkStories[currentElement].options[i];
+  }
+}
 
-    button.innerText = games.darkStories[currentElement].options[i];
-
-    gameBoard.appendChild(button);
-    button.setAttribute("class", "allBtn");
-    // console.log(button)
-
-    button.addEventListener("click", () => {
-      const clickedBtn = event.target.innerText;
-
-      // if (clickedBtn === games.darkStories[currentElement].correctAnswer) {
-      // goToNext()
-      //   confirmBtn.classList.remove("hide");
-      //    console.log(clickedBtn)
-      // console.log(games.darkStories[currentElement].correctAnswer)
-    });
+function checkQuestion(option) {
+  if (option === games.darkStories[currentElement].correctAnswer) {
+    console.log("acertou");
+  } else {
+    console.log("sefu");
   }
 }
